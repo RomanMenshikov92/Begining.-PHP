227f58e8cad2d11a6e11b6d89855c69a46e3d7c9
Роман Меньшиков <81389495+RomanMenshikov92@users.noreply.github.com>
2021-04-15 00:46:54 +0300

    PHP

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$title = 'Мой сайт';
+$name = 'R-Pac';
+$image = 'img/2pac-logo.svg';
+$email = 'R-Tupac@yandex.ru';
+$phone = '+79997149382';
+
+require('components/header.php');
+require('components/status.php');
+require('components/about.php');
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
